ba9afb89bcdc7b74b02a4f6dfb37315cce54ceb3
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-06-11 10:09:36 +0200

    original kernel modules and generic libaudio sources
    libaudio, libaudiopolicy and tools based on aries-common
    
    Change-Id: Ib2aa130bf835deda72d2e82b11cfbb6453ab1fcc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,6 @@
-USE_CAMERA_STUB := false
 
 TARGET_BOOTLOADER_BOARD_NAME := GT-I9103
+#TARGET_OTA_ASSERT_DEVICE := galaxyr,GT-I9103
 
 TARGET_NO_BOOTLOADER := true
 TARGET_BOARD_PLATFORM := tegra
@@ -19,22 +19,33 @@ TARGET_KERNEL_MODULES_EXT := device/samsung/i9103/modules
 TARGET_KERNEL_UTSRELEASE  := 2.6.36.3
 KERNEL_SRC_DIR := $(ANDROID_BUILD_TOP)/kernel/samsung/i9103
 
+# do not update the device tree...
+DO_NO_REBUILT_THE_KERNEL := 1
+
 #TARGET_NO_BOOT := false
 
 # use a prebuilt init.rc for the first roms
 TARGET_PROVIDES_INIT_RC := true
-
-#TARGET_NO_RECOVERY := false
-#TARGET_RECOVERY_INITRC := ./device/samsung/i9103/recovery/init.rc
+TARGET_NO_RECOVERY := false
+TARGET_RECOVERY_INITRC := ./device/samsung/i9103/recovery/init.rc
 TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
 BOARD_HAS_SMALL_RECOVERY := true
+#BOARD_CUSTOM_RECOVERY_KEYMAPPING:= ../../device/samsung/i9103/recovery/recovery_ui.c
 
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
+#BOARD_SYSTEMIMAGE_PARTITION_SIZE := 629145600
+#BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 209715200 # let them small, we use a zip... 200M
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 104857600 # must be a multiple of the block size
+BOARD_FLASH_BLOCK_SIZE := 4096
 BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 BOARD_USES_MMCUTILS := true
+
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
 
-#TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_EXT4 := true
 
 # Wifi
 BOARD_WPA_SUPPLICANT_DRIVER := WEXT
@@ -52,8 +63,22 @@ WIFI_DRIVER_FW_AP_PATH      := "/system/etc/wifi/bcm4330_aps.bin"
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
 
+# Camera
+USE_CAMERA_STUB := false
+BOARD_CAMERA_LIBRARIES := libcamera
+BOARD_SECOND_CAMERA_DEVICE := true
+
+# Audio
+BOARD_USES_GENERIC_AUDIO := false
+TARGET_PROVIDES_LIBAUDIO := false # build them
+
+# Usb
+BOARD_USE_USB_MASS_STORAGE_SWITCH := true
+BOARD_UMS_LUNFILE := "/sys/devices/platform/usb_mass_storage/lun0/file"
+
 # EGL
 BOARD_EGL_CFG := device/samsung/i9103/etc/egl.cfg
 
 # Custom squisher, final step script
 TARGET_CUSTOM_RELEASETOOL := ./device/samsung/i9103/releasetools/squisher
+
--- device_i9103.mk
+++ device_i9103.mk
@@ -1,20 +1,44 @@
+###############################################################################
+# Device Packages
+###############################################################################
+
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_eu_supl.mk)
 
+###############################################################################
+
 $(call inherit-product-if-exists, vendor/samsung/i9103/i9103-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/samsung/i9103/overlay
+###############################################################################
 
+# device modules
+PRODUCT_PACKAGES += libaudio libaudiopolicy amix aplay arec
+
+# external packages
 PRODUCT_PACKAGES += hcitool hciconfig
 
+###############################################################################
+
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-        LOCAL_KERNEL := device/samsung/i9103/kernel
+    LOCAL_KERNEL := device/samsung/i9103/kernel
 else
-        LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+    LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
 PRODUCT_COPY_FILES += $(LOCAL_KERNEL):kernel
+
+# copy all others kernel modules under the "modules" dir. to system/lib/modules
+PRODUCT_COPY_FILES += $(shell test -d device/samsung/i9103/modules && \
+        find device/samsung/i9103/modules -name '*.ko' \
+        -printf '%p:system/lib/modules/%f ')
+
+###############################################################################
+
 $(call inherit-product, device/samsung/i9103/blobs_i9103.mk)
+
+###############################################################################
+
+DEVICE_PACKAGE_OVERLAYS += device/samsung/i9103/overlay
 
 $(call inherit-product, build/target/product/full_base.mk)
 
@@ -23,3 +47,4 @@ PRODUCT_LOCALES += hdpi
 
 PRODUCT_NAME := full_i9103
 PRODUCT_DEVICE := i9103
+
